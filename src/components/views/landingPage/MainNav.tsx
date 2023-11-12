50588ca6b7df290b739bf81a75aecfd8787229cb
arfin-web <arfinpriom12@gmail.com>
2023-11-12 16:34:54 +0600

    shadcn ui bug fixed

---
+++ src/components/views/landingPage/MainNav.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from '@/components/ThemeSwitcher'
+import Link from "next/link"
+
+const MainNav = () => {
+    return (
+        <>
+            <div className="container mx-auto px-2 lg:px-20">
+                <nav className="flex flex-wrap items-center justify-between py-4 md:py-6 ">
+                    <Link href="/" className="text-xl font-bold">Logo</Link>
+                    <div className="flex justify-center items-center gap-4">
+                        <Button size='lg'>Try It Now</Button>
+                        <ModeToggle />
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+export default MainNav
